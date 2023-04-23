65fde440ae78d9c92c16ce018cdacd47c0dc9d44
Esraa Syam <e294014@gmail.com>
2023-04-23 13:36:14 +0200

    update

--- backslash.c
+++ backslash.c
@@ -1,18 +1,34 @@
 #include "functions.h"
 
 /**
- * backslash - "%f /t //"
+ * backslash - check the character after a backslash
+ * @format: format string
+ * @i: index of format string
+ * Return: 1 if character is valid, 0 if not
 */
 
 int backslash(char *format, int i)
 {
 	char c = format[i + 1];
-	if (c && (c == '\\' || c == "\"" || c == "\'" || c == "%"))
+	if (c && (c == '\\' || c == '\"' || c == '\'' || c == '%'))
 	{
-		print_per(c);
+		print_any_char(c);
 		return (1);
 	}
-	write(1, "\"+'c, 1);
-	return (1);
-
+	/* check if it's \n , \t , and so on and print them */
+	if (c && (c == 'n' || c == 't' || c == 'r' || c == 'b' || c == 'f'))
+	{
+		if (c == 'n')
+			print_any_char('\n');
+		if (c == 't')
+			print_any_char('\t');
+		if (c == 'r')
+			print_any_char('\r');
+		if (c == 'b')
+			print_any_char('\b');
+		if (c == 'f')
+			print_any_char('\f');
+		return (1);
+	}
+	return (0);
 }
